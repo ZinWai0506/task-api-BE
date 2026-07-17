f9da1a39dce8d75b4346d4c2f7d084de3dd43dba
Zin Min Wai <zinminwai.jeffrey@gmail.com>
2026-07-17 10:54:34 -0400

    frontend deployed

---
+++ task-api-frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([])
+  const [tasks, setTasks] = useState([])
+  const BASE_API_URL = import.meta.env.VITE_API_URL || 'https://task-api-be.onrender.com'
+
+  useEffect(() => {
+    fetch(BASE_API_URL + '/api/users')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setUsers(data)
+      })
+    fetch(BASE_API_URL + '/api/tasks')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setTasks(data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h1>My Deployed App!</h1>
+
+      {
+        users.map((u) => (
+          <p key={u.id}>User: {u.name}</p>
+        ))
+      }
+      {
+        tasks.map((t) => (
+          <p key={t.id}>Task: {t.title}</p>
+        ))
+      }
+    </div>
+  )
+}
+
+export default App
